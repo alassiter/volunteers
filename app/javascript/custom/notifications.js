85fc7c8b2bccf2f9249d0b61607705a5c8378b8b
alassiter <anthony.lassiter@getbraintree.com>
2019-10-11 16:18:42 -0400

    Clean up

--- app/javascript/custom/notifications.js
+++ app/javascript/custom/notifications.js
@@ -2,8 +2,4 @@ document.addEventListener('turbolinks:load', function(){
   setTimeout(function() {
     document.getElementsByClassName('notification')[0].className += " fade";
   }, 3000);
-
-  // setTimeout(function() {
-  //   document.getElementById('notifications').style.display = "none";
-  // }, 4000);
 })
